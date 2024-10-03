bf8d3926513ff6babb1e94251f8e4c6417aedd02
Wendell S. <123216078+wendell-S@users.noreply.github.com>
2024-10-03 16:41:28 -0300

    Add upgrade v0.4

--- Plataforma de Gestão de Tarefas TaskFlow/src/js/index.js
+++ Plataforma de Gestão de Tarefas TaskFlow/src/js/index.js
@@ -3,6 +3,10 @@ const $descriçaoInput = document.getElementById('descript')
 const $prioridadeInput = document.getElementById('prioridade')
 const $deadline = document.getElementById('deadline')
 
+const $todoColumnBody = document.querySelector('#todoColumn .body')
+
+let todoList = [];
+
 function abrirModal() {
     $modal.style.display = "flex"
 }
@@ -11,10 +15,45 @@ function fecharModal() {
     $modal.style.display = "none"
 }
 
+function generateCards() {
+    const todoListHtml = todoList.map(function(task){
+        const formatarDate = moment(task.deadline).format('DD/MM/YYYY')
+        return `
+          <div class="card">
+             <div class="info">
+                <b>Descriçao</b>
+                <span>${task.descriçao}</span>
+             </div>
+             
+             <div class="info">
+                <b>prioridade</b>
+                <span>${task.prioridade}</span>
+             </div>
+
+             <div class="info">
+                <b>prazo</b>
+                <span>${formatarDate}</span>
+             </div>
+          </div>
+        `;
+    })
+
+    $todoColumnBody.innerHTML = todoListHtml.join('')
+}
+
 function criarTask() {
-    console.log($descriçaoInput.value)
-    console.log($prioridadeInput.value)
-    console.log($deadline.value)
+
+    const newTask = {
+        descriçao: $descriçaoInput.value,
+        prioridade: $prioridadeInput.value,
+        deadline: $deadline.value,
+    }
+
+    todoList.push(newTask)
+
+
 
     fecharModal()
+    generateCards()
 }
+
